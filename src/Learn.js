9b2995dd83023d247adbd50509fb92eaf56ff6dc
Elisabeth Nguyen <elisabethnguyen@my.unt.edu>
2025-01-26 03:27:16 -0600

    learn

--- src/Learn.js
+++ src/Learn.js
@@ -10,7 +10,7 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import MenuIcon from '@mui/icons-material/Menu';
 
-function App() {
+function Learn() {
   const [anchorEl, setAnchorEl] = useState(null);
 
   const handleMenuClick = (event) => {
@@ -29,7 +29,7 @@ function App() {
             Ember
           </Typography>
           <Typography variant="h6" style={{ flexGrow: 1 }}>
-            Home
+            Learn About Fires
           </Typography>
           <IconButton edge="end" color="inherit" aria-label="menu" onClick={handleMenuClick}>
             <MenuIcon />
@@ -39,17 +39,24 @@ function App() {
             open={Boolean(anchorEl)}
             onClose={handleMenuClose}
           >
-            <MenuItem onClick={() => { handleMenuClose(); window.location.href = '/Port'; }}>Damages Report</MenuItem>
-            <MenuItem onClick={() => { handleMenuClose(); window.location.href = '/Learn'; }}>Learn About Fires</MenuItem>
+            <MenuItem onClick={() => { handleMenuClose(); window.location.href = '/port'; }}>Damages Report</MenuItem>
+            <MenuItem onClick={handleMenuClose}>Learn About Fires</MenuItem>
           </Menu>
         </Toolbar>
       </AppBar>
       <div className="content">
-        <Typography variant="h4" gutterBottom>
-          Ember
+        <Typography variant="h5" gutterBottom>
+          What Causes Wildfires?
         </Typography>
         <Typography variant="body1" paragraph>
-          Fire is a rapid oxidation of a material in the exothermic chemical process of combustion, releasing heat, light, and various reaction products. Fires can cause physical damage through burning.
+          The common causes of wildfires include the following:<br></br> 
+            - unattended campfires <br></br>
+            - machinery malfunctions and vehicle fires<br></br> 
+            - lightning strikes <br></br>
+            - cigarette butts <br></br>
+            - burning of debris <br></br>
+            - and occasionally, arson<br></br> 
+          As seen in the list above, sometimes the smallest sparks can set off devastatingly large fires.
         </Typography>
         <Typography variant="body1" paragraph>
           Wildfires are uncontrolled fires that spread quickly and can cause significant damage to forests, wildlife, and human structures. They are often caused by lightning strikes or human activities.
@@ -65,4 +72,4 @@ function App() {
   );
 }
 
-export default App;
+export default Learn;
