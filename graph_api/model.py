86164205310402fb6b34d0ab4bff3ce8dc4d8d77
premiumfriedrice <lloyddalba@gmail.com>
2025-01-26 05:45:43 -0600

    Fixed width of plot

--- graph_api/model.py
+++ graph_api/model.py
@@ -5,7 +5,7 @@ import os
 
 genai.configure(api_key=os.getenv("GENAI_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash",
-            system_instruction="You are a data analyst predicting the cost of damage by fire in a certain area given relevant data in json format. Please analyze this data and provide insights, such as: summary of key statistics, patterns or trends, anomalies or outliers, and interesting insights."
+        system_instruction="You are a data analyst predicting the cost of damage by fire in a certain area given relevant data in json format. Please analyze this data and provide insights, such as: summary of key statistics, patterns or trends, anomalies or outliers, and interesting insights."
     )
 response = model.generate_content("What is the estimated cost of damage by fire? Here is the data in a json format: " + data_parse().to_json(orient='records'))
 print(response.text)
