e8acdb4590b8d8f6ad9f9d83a6515852f47de18c
Ayad Masud <ayad.r.masud@gmail.com>
2025-01-25 19:10:36 -0600

    fixing api keys

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from data_parse import data_parse
 import google.generativeai as genai
+import os
 
-genai.configure(api_key="AIzaSyCEqhPm_XqTkMCtD2SgNEnRTY9xEnaOu6U")
+genai.configure(api_key=os.getenv("GENAI_API_KEY"))
 model = genai.GenerativeModel("gemini-1.5-flash",
             system_instruction="You are a data analyst predicting the cost of damage by fire in a certain area given relevant data in json format. Please analyze this data and provide insights, such as: summary of key statistics, patterns or trends, anomalies or outliers, and interesting insights."
     )
@@ -19,24 +20,3 @@ print(response.text)
 
 
 
-
-# import openai
-
-# openai.api_key = "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"
-
-# data = data_parse() # get data
-# data_json = data.to_json(orient='records') # convert to json format
-
-
-# response = openai.chat.completions.create(
-#     model="gpt-4o-mini",
-#     messages=[
-#         {"role": "analyst", "content": "You are a data analyst predicting the cost of damage by fire in a certain area given relevant data."},
-#         {
-#             "role": "user",
-#             "content": "Please tell me what data science is?"
-#         }
-#     ]
-# )
-
-# print(response.choices[0].message)
